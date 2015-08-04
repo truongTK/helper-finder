7e6d6770575a8f1a25191caeaeac18d2d851e220
root <linhnh@twin.vn>
2015-08-04 10:48:36 +0700

    Add LeftNav to Home Page and Customer Page

--- client/home-page/home-page.jsx
+++ client/home-page/home-page.jsx
@@ -1,9 +1,38 @@
+/**
+* @Description: Home Page
+* @Author: linhnh
+*/
+
 const{
   AppBar,
+  LeftNav,
+  MenuItem,
   RaisedButton,
   CardMedia
 } = mui;
 const ThemeManager = new mui.Styles.ThemeManager();
+
+menuItems = [
+  { route: 'get-started', text: 'Get Started' },
+  { route: 'customization', text: 'Customization' },
+  { route: 'components', text: 'Components' },
+  { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+  {
+     type: MenuItem.Types.LINK,
+     payload: 'https://github.com/callemall/material-ui',
+     text: 'GitHub'
+  },
+  {
+     text: 'Disabled',
+     disabled: true
+  },
+  {
+     type: MenuItem.Types.LINK,
+     payload: 'https://www.google.com',
+     text: 'Disabled Link',
+     disabled: true
+  },
+];
 
 HomePage = React.createClass({
   childContextTypes: {
@@ -16,6 +45,10 @@ HomePage = React.createClass({
     };
   },
 
+  onOpenLeftNav(){
+    this.refs.leftNav.toggle();
+  },
+
   onCustomerClick(){
     React.render(<Customer />, document.getElementById("render-target"));
   },
@@ -26,7 +59,8 @@ HomePage = React.createClass({
         <AppBar
           title=""
           iconClassNameRight="muidocs-icon-navigation-expand-more"
-          showMenuIconButton={false} />
+          onLeftIconButtonTouchTap={this.onOpenLeftNav} />
+        <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
         <br/>
         <div className="button-secondary">
           <RaisedButton
